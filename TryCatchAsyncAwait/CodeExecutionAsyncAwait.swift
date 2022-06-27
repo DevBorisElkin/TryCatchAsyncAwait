e862712e96e80ea098279800a5ed83e08e29e929
DevBorisElkin <ea.foreverrr@gmail.com>
2022-06-28 00:14:06 +0300

    more changes

---
+++ TryCatchAsyncAwait/CodeExecutionAsyncAwait.swift
@@ -0,0 +1,13 @@
+//
+//  CodeExecutionAsyncAwait.swift
+//  TryCatchAsyncAwait
+//
+//  Created by test on 28.06.2022.
+//
+
+import Foundation
+
+// check this https://www.youtube.com/watch?v=SedBsBo5mhA&ab_channel=SwiftBook
+public func executeSomeAsyncCode(){
+    
+}
