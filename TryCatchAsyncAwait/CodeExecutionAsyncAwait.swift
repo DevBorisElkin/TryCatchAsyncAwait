58bbd1c9431b2a7aed4959ae651048727636e1bb
DevBorisElkin <ea.foreverrr@gmail.com>
2022-06-29 23:32:41 +0300

    backup

--- TryCatchAsyncAwait/CodeExecutionAsyncAwait.swift
+++ TryCatchAsyncAwait/CodeExecutionAsyncAwait.swift
@@ -7,7 +7,51 @@
 
 import Foundation
 
-// check this https://www.youtube.com/watch?v=SedBsBo5mhA&ab_channel=SwiftBook
-public func executeSomeAsyncCode(){
+class Vegetable{
+    let id: Int
+    var alreadyChopped = false
+
+    init(id: Int){
+        self.id = id
+    }
     
+    func chopVegetable() async throws -> Vegetable{
+        print("Vegetable \(id) is starting to get chopped")
+        alreadyChopped = true
+        Thread.sleep(forTimeInterval: 2)
+        print("Vegetable \(id) finally got chopped")
+        return self
+    }
+}
+
+func gatherRawVeggies() -> [Vegetable]{
+    [Vegetable(id: 1), Vegetable(id: 2), Vegetable(id: 3)]
+}
+
+func chopVegetables() async throws -> [Vegetable]{
+    let rawVeggies = gatherRawVeggies()
+    var choppedVeggies = [Vegetable]()
+    
+    for rawVegetable in rawVeggies{
+        choppedVeggies.append(try await rawVegetable.chopVegetable())
+    }
+    return choppedVeggies
+}
+
+// check this https://www.youtube.com/watch?v=SedBsBo5mhA&ab_channel=SwiftBook
+// unfortunately deprecated code, but it's worth studying
+//public func executeSomeAsyncCode(){
+//    async{
+//        let veggies = try await chopVegetables()
+//        for veggie in veggies {
+//            print("Vegetable[\(veggie.id)] is chopped[\(veggie.alreadyChopped)]")
+//        }
+//    }
+//}
+
+public func executeSomeAsyncCode() async {
+    let veggies = try await chopVegetables()
+    for veggie in veggies {
+        print("Vegetable[\(veggie.id)] is chopped[\(veggie.alreadyChopped)]")
+    }
 }
